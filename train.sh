d39dea70f813200e91294a91e36c07104e763b4d
anhv <anhv.ict91@gmail.com>
2018-12-21 20:27:19 +0700

    remove evaluation

--- train.sh
+++ train.sh
@@ -5,7 +5,7 @@
 
 make
 
-CORPUS=/home/anhv/PycharmProjects/undertheseanlp/word_embeddings/egs/word2vec/data/corpus_1.8G.normalized.txt
+CORPUS=/home/anhv/PycharmProjects/undertheseanlp/word_embeddings/egs/word2vec/data/corpus_18G.normalized.txt
 VOCAB_FILE=vocab.txt
 COOCCURRENCE_FILE=cooccurrence.bin
 COOCCURRENCE_SHUF_FILE=cooccurrence.shuf.bin
@@ -31,16 +31,6 @@ if [[ $? -eq 0 ]]
     if [[ $? -eq 0 ]]
     then
        $BUILDDIR/glove -save-file $SAVE_FILE -threads $NUM_THREADS -input-file $COOCCURRENCE_SHUF_FILE -x-max $X_MAX -iter $MAX_ITER -vector-size $VECTOR_SIZE -binary $BINARY -vocab-file $VOCAB_FILE -verbose $VERBOSE
-       if [[ $? -eq 0 ]]
-       then
-           if [ "$1" = 'matlab' ]; then
-               matlab -nodisplay -nodesktop -nojvm -nosplash < ./eval/matlab/read_and_evaluate.m 1>&2 
-           elif [ "$1" = 'octave' ]; then
-               octave < ./eval/octave/read_and_evaluate_octave.m 1>&2 
-           else
-               python eval/python/evaluate.py
-           fi
-       fi
     fi
   fi
 fi

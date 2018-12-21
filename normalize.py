1190851c818bd3c1ec907dfebae9060f9e6ccc3d
anhv <anhv.ict91@gmail.com>
2018-12-21 20:32:48 +0700

    update normalize

--- normalize.py
+++ normalize.py
@@ -2,9 +2,8 @@ from os import remove
 from os.path import exists
 from datetime import datetime
 from underthesea.word_tokenize import tokenize
-from time import gmtime, strftime
 
-input = "/media/anhv/data/nlp-data/vncorpus 18GB/corpus-full.txt"
+input_filepath = "/media/anhv/data/nlp-data/vncorpus 18GB/corpus-full.txt"
 output_filepath = "/home/anhv/PycharmProjects/undertheseanlp/word_embeddings/egs/word2vec/data/corpus_18G.normalized.txt"
 if exists(output_filepath):
     remove(output_filepath)
@@ -13,5 +12,5 @@ for i, text in enumerate(open(input)):
     if i % 100000 == 0:
         print(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), i)
     text = text.strip()
-    text = tokenize(text)
+    text = tokenize(text) + "\n"
     output.write(text)
